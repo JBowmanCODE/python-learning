df91982c5f7b73adee8fa9f0ad76d7eadca09c84
JBowmanCODE <ukjbowman@gmail.com>
2026-02-21 16:43:44 +0000

    foor loops

--- forloops.py
+++ forloops.py
@@ -1,11 +1,11 @@
-#Asks the user to enter a number
-#Uses a loop to print every number from 1 up to that number
-#But if the number is divisible by 3, print "Fizz" instead
-
+# Ask the user to enter a number and convert it to an integer
 number = int(input("Enter a number: "))
 
+# Loop from 1 up to and including the number the user entered
 for i in range(1, number + 1):
+    
+    # Check if the current number is divisible by 3 (no remainder)
     if i % 3 == 0:
-        print("Fizz")
+        print("Fizz")  # Print Fizz instead of the number
     else:
-        print(i)
+        print(i)  # Otherwise just print the number
